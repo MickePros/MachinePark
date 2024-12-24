9890ecaa006cb8ba737875e2db0217a2f5092755
Mikael Pros <Mikael_Pros@hotmail.com>
2024-12-24 11:47:20 +0100

    Add functionality to add new machines

--- MachinePark/Model/Machine.cs
+++ MachinePark/Model/Machine.cs
@@ -1,9 +1,13 @@
-﻿namespace MachinePark.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MachinePark.Model
 {
     public class Machine
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Location is required")]
+        [StringLength(25, ErrorMessage = "Location is too long")]
         public string Location { get; set; }
         public DateTime Edited { get; set; }
         public bool Status { get; set; }
