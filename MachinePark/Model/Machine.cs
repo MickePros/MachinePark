623ba4a33c62976485072fc59f6367dfed70d702
Mikael Pros <Mikael_Pros@hotmail.com>
2024-12-24 10:45:58 +0100

    Add some Styling, Add button functionality to Machines

--- MachinePark/Model/Machine.cs
+++ MachinePark/Model/Machine.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; }
         public string Location { get; set; }
         public DateTime Edited { get; set; }
-        public int Status { get; set; }
+        public bool Status { get; set; }
     }
 }
